baede9c23a0fa8ffcd4cb13ea5dc535fdccf1d9a
QIanZhang1218 <zhang_qianzqzq@163.com>
2022-08-11 16:53:31 +1200

    fetch data from data.json file

--- coding-challenge/src/Hooks/useFetchData.ts
+++ coding-challenge/src/Hooks/useFetchData.ts
@@ -1,5 +1,25 @@
 import {useState,useEffect} from "react"
 
 export const useFetchData = () => {
-    
+    const [data,setData] = useState([]);
+
+    const fetchData = () =>{
+        //fetch data
+        fetch("./data,json",{
+            headers: {
+                "content-type": "application/json;charset=UTF-8",
+              },
+        }).then(
+            res => res.json()
+        ).then(
+            jsonData => {
+                setData(jsonData);
+            }
+        )
+    }
+    useEffect(() => {
+        fetchData();
+    },[]);
+    console.log(data);
+    return data;
 }
