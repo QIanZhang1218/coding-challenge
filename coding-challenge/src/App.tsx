cabc7416b3c2a8b121e81e3e524582b0f2674d7e
QIanZhang1218 <zhang_qianzqzq@163.com>
2022-08-11 16:56:58 +1200

    import useFecthData hook into app.ts file

--- coding-challenge/src/App.tsx
+++ coding-challenge/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { useFetchData } from "./Hooks/useFetchData";
 
 function App() {
+  const data = useFetchData();
   return (
     <div className="App">
       <h1>9Spokes Coding-Challenge:</h1>
