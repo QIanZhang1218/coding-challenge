d4eaf3ea4ba0d180aeebb07b872cb598b39b8447
QIanZhang1218 <zhang_qianzqzq@163.com>
2022-08-11 17:19:45 +1200

    Create useExpenseCalculator hook

--- coding-challenge/src/App.tsx
+++ coding-challenge/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { useFetchData } from "./Hooks/useFetchData";
 import { useRevenueCalculator } from "./Hooks/useRevenueCalculator";
+import { useExpenseCalculator } from "./Hooks/useExpenseCalculator";
 
 function App() {
   const data = useFetchData();
   const totalRevenue = useRevenueCalculator(data);
+  const totalExpense = useExpenseCalculator(data);
   return (
     <div className="App">
       <h1>9Spokes Coding-Challenge:</h1>
---
+++ coding-challenge/src/Hooks/useExpenseCalculator.ts
@@ -0,0 +1,3 @@
+export const useExpenseCalculator = (data:any[]) => {
+    console.log(data);
+}
