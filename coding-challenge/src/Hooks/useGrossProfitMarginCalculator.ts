23b23422b75375f774f7ede02f65c0bd72b0cf95
QIanZhang1218 <zhang_qianzqzq@163.com>
2022-08-11 17:38:00 +1200

    Calculate total sales

--- coding-challenge/src/Hooks/useGrossProfitMarginCalculator.ts
+++ coding-challenge/src/Hooks/useGrossProfitMarginCalculator.ts
@@ -1,3 +1,10 @@
 export const useGrossProfitMargin = (data:any[],revenue:number) => {
-    console.log(data,revenue);
+    let totalSales:number = 0;
+    data.forEach(item=>{
+        //Calculate total sales
+        if(item.account_category === "sales" && item.value_type === "debit"){
+            totalSales += item.total_value;
+        }
+    })
+    console.log(totalSales);
 }
